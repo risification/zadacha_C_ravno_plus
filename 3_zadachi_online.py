75f1367556e8640bde3ac9dd7c9145ef0b7b01cd
risification <sultangaziev01@bk.ru>
2020-12-19 01:06:28 +0600

    c+=

--- 3_zadachi_online.py
+++ 3_zadachi_online.py
@@ -10,20 +10,17 @@ elif len(string) == 1 and string.islower():
     print(string.swapcase())
 else:
     print(string)
-# 3
+# 2
 T = int(input())
-step = 0
 i = 0
 while i < T:
-    a_b_c = list(map(int, input().split()))
-    a = a_b_c[0]
-    b = a_b_c[1]
-    c = a_b_c[2]
+    step = 0
+    a, b, c = list(map(int, input().split()))
     i += 1
-    while a <= c or b <= c:
-        b += a
-        a = b - a
+    while max(a, b) < c + 1:
+        if a < b:
+            a += b
+        else:
+            b += a
         step += 1
-    else:
-        print(step - 1)
-        step = 0
+    print(step)
